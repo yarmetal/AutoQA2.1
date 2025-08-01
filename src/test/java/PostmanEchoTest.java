54cb99ddb0ccc3bdb1725dfa64222cceff47aec2
BZza <bzza_yar87@mail.ru>
2025-08-01 23:22:49 +0300

    InitialCommit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+
+public class PostmanEchoTest {
+
+
+    @Test
+    void postmanEchoTest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Какой сегодня день?")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Какой сегодня день?"))
+        ;
+
+
+    }
+}
